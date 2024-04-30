41dee9f3540a6752c88a451b6163110da435b3d0
Adam Hemer <adamhemer32@gmail.com>
2024-04-30 22:35:03 +0930

    Remvoing build files

--- build/sketch/README.md
+++
@@ -1,2 +0,0 @@
-#line 1 "C:\\Users\\Adam\\Documents\\dev\\rock-cheque-v2-control\\README.md"
-# rock-cheque-v2-control
--- build/sketch/RockCheque.cpp
+++
@@ -1,4 +0,0 @@
-#line 1 "C:\\Users\\Adam\\Documents\\dev\\rock-cheque-v2-control\\RockCheque.cpp"
-#include "RockCheque.h"
-#include "Arduino.h"
-#include "Wire.h"
